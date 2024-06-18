f3650f0c89e1af449d30ba321868de819c6de5a6
Aphyla <aphyla@aphyla.com>
2024-06-18 23:12:54 +0200

    Remove unused error

--- contracts/ccip/CCIPBaseUpgradeable.sol
+++ contracts/ccip/CCIPBaseUpgradeable.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.20;
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 abstract contract CCIPBaseUpgradeable is AccessControlUpgradeable {
-    error CCIPBaseOnlyCCIPRouter();
-
     address public immutable CCIP_ROUTER;
 
     function __CCIPBase_init() internal onlyInitializing {}
