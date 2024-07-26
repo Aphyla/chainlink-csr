8b00a1ab71ee8ff2f58f14cd695906c03f66f432
Aphyla <aphyla@aphyla.com>
2024-07-26 19:58:58 +0200

    Add Eigenpie deploy script

---
+++ script/eigenpie/EigenpieDeploy.s.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import "./EigenpieParameters.sol";
+
+import "../../contracts/senders/CustomSender.sol";
+import "../../contracts/receivers/EigenpieCustomReceiver.sol";
+import "../../contracts/adapters/CCIPAdapter.sol";
+import "../../contracts/automations/SyncAutomation.sol";
+import "../../contracts/utils/OraclePool.sol";
+import "../../contracts/utils/PriceOracle.sol";
+
+contract EigenpieDeployScript is Script, EigenpieParameters {
+    struct Proxy {
+        address proxy;
+        address proxyAdmin;
+        address implementation;
+    }
+
+    struct L1Contracts {
+        string chainName;
+        Proxy receiver;
+        address arbitrumAdapter;
+    }
+
+    struct L2Contracts {
+        string chainName;
+        Proxy sender;
+        address priceOracle;
+        address oraclePool;
+        address syncAutomation;
+    }
+
+    uint256 public ethereumForkId;
+    uint256 public arbitrumForkId;
+
+    address public deployer;
+
+    function setUp() public {
+        ethereumForkId = vm.createFork(vm.rpcUrl("ethereum"));
+        arbitrumForkId = vm.createFork(vm.rpcUrl("arbitrum"));
+    }
+
+    function run() public returns (L1Contracts memory l1Contracts, L2Contracts[] memory l2Contracts) {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        deployer = vm.addr(deployerPrivateKey);
+
+        l1Contracts.chainName = "Ethereum";
+
+        l2Contracts = new L2Contracts[](1);
+
+        L2Contracts memory arbContracts = l2Contracts[0];
+
+        arbContracts.chainName = "Arbitrum";
+
+        // Deploy contracts on Ethereum
+        {
+            vm.selectFork(ethereumForkId);
+            vm.startBroadcast(deployerPrivateKey);
+
+            l1Contracts.receiver.implementation = address(
+                new EigenpieCustomReceiver(
+                    ETHEREUM_EGETH_TOKEN, ETHEREUM_EGETH_STAKING, ETHEREUM_WETH_TOKEN, ETHEREUM_CCIP_ROUTER, address(0)
+                )
+            );
+
+            l1Contracts.receiver.proxy = address(
+                new TransparentUpgradeableProxy(
+                    l1Contracts.receiver.implementation,
+                    deployer,
+                    abi.encodeCall(EigenpieCustomReceiver.initialize, (deployer))
+                )
+            );
+
+            l1Contracts.receiver.proxyAdmin = _getProxyAdmin(l1Contracts.receiver.proxy);
+
+            l1Contracts.arbitrumAdapter =
+                address(new CCIPAdapter(ETHEREUM_EGETH_TOKEN, ETHEREUM_CCIP_ROUTER, l1Contracts.receiver.proxy));
+            vm.stopBroadcast();
+        }
+
+        // Deploy contracts on Arbitrum
+        {
+            vm.selectFork(arbitrumForkId);
+            vm.startBroadcast(deployerPrivateKey);
+
+            arbContracts.priceOracle = address(
+                new PriceOracle(
+                    ARBITRUM_EGETH_ETH_DATAFEED,
+                    ARBITRUM_EGETH_ETH_DATAFEED_IS_INVERSE,
+                    ARBITRUM_EGETH_ETH_DATAFEED_HEARTBEAT,
+                    deployer
+                )
+            );
+
+            arbContracts.oraclePool = address(
+                new OraclePool(
+                    _predictContractAddress(deployer, 2), // As we deploy this contract, the impementation and then the proxy, we need to increment the nonce by 2
+                    ARBITRUM_WETH_TOKEN,
+                    ARBITRUM_EGETH_TOKEN,
+                    arbContracts.priceOracle,
+                    ARBITRUM_ORACLE_POOL_FEE,
+                    deployer
+                )
+            );
+
+            arbContracts.sender.implementation = address(
+                new CustomSender(ARBITRUM_WETH_TOKEN, ARBITRUM_LINK_TOKEN, ARBITRUM_CCIP_ROUTER, address(0), address(0))
+            );
+
+            arbContracts.sender.proxy = address(
+                new TransparentUpgradeableProxy(
+                    arbContracts.sender.implementation,
+                    deployer,
+                    abi.encodeCall(CustomSender.initialize, (arbContracts.oraclePool, deployer))
+                )
+            );
+
+            arbContracts.sender.proxyAdmin = _getProxyAdmin(arbContracts.sender.proxy);
+
+            arbContracts.syncAutomation =
+                address(new SyncAutomation(arbContracts.sender.proxy, ETHEREUM_CCIP_CHAIN_SELECTOR, deployer));
+
+            vm.stopBroadcast();
+        }
+
+        // Set up contracts on ethereum
+        {
+            vm.selectFork(ethereumForkId);
+            vm.startBroadcast(deployerPrivateKey);
+
+            EigenpieCustomReceiver(payable(l1Contracts.receiver.proxy)).setAdapter(
+                ARBITRUM_CCIP_CHAIN_SELECTOR, l1Contracts.arbitrumAdapter
+            );
+            EigenpieCustomReceiver(payable(l1Contracts.receiver.proxy)).setSender(
+                ARBITRUM_CCIP_CHAIN_SELECTOR, abi.encode(arbContracts.sender.proxy)
+            );
+
+            vm.stopBroadcast();
+        }
+
+        // Set up contracts on Arbitrum
+        {
+            vm.selectFork(arbitrumForkId);
+            vm.startBroadcast(deployerPrivateKey);
+
+            CustomSender(arbContracts.sender.proxy).setReceiver(
+                ETHEREUM_CCIP_CHAIN_SELECTOR, abi.encode(l1Contracts.receiver.proxy)
+            );
+
+            CustomSender(arbContracts.sender.proxy).grantRole(keccak256("SYNC_ROLE"), arbContracts.syncAutomation);
+
+            vm.stopBroadcast();
+        }
+    }
+
+    function _predictContractAddress(address account, uint256 deltaNonce) private view returns (address) {
+        uint256 nonce = vm.getNonce(account) + deltaNonce;
+        return vm.computeCreateAddress(account, nonce);
+    }
+
+    function _getProxyAdmin(address proxy) private view returns (address) {
+        return address(uint160(uint256(vm.load(proxy, ERC1967Utils.ADMIN_SLOT))));
+    }
+}
---
+++ test/script/eigenpie/EigenpieDeployScript.t.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import "../../../script/Eigenpie/EigenpieDeploy.s.sol";
+
+contract EigenpieDeployScriptTest is Test, EigenpieParameters {
+    EigenpieDeployScript public script;
+
+    function setUp() public {
+        script = new EigenpieDeployScript();
+        script.setUp();
+    }
+
+    function test_Deploy() public {
+        (EigenpieDeployScript.L1Contracts memory l1Contracts, EigenpieDeployScript.L2Contracts[] memory l2Contracts) =
+            script.run();
+
+        address deployer = script.deployer();
+
+        vm.selectFork(script.ethereumForkId());
+
+        {
+            EigenpieCustomReceiver receiver = EigenpieCustomReceiver(payable(l1Contracts.receiver.proxy));
+
+            assertEq(receiver.EGETH(), ETHEREUM_EGETH_TOKEN, "test_Deploy::1");
+            assertEq(receiver.WNATIVE(), ETHEREUM_WETH_TOKEN, "test_Deploy::2");
+            assertEq(receiver.CCIP_ROUTER(), ETHEREUM_CCIP_ROUTER, "test_Deploy::3");
+            assertEq(receiver.hasRole(receiver.DEFAULT_ADMIN_ROLE(), deployer), true, "test_Deploy::4");
+            assertEq(receiver.getAdapter(ARBITRUM_CCIP_CHAIN_SELECTOR), l1Contracts.arbitrumAdapter, "test_Deploy::5");
+            assertEq(
+                receiver.getSender(ARBITRUM_CCIP_CHAIN_SELECTOR),
+                abi.encode(l2Contracts[0].sender.proxy),
+                "test_Deploy::7"
+            );
+            assertEq(_getProxyAdmin(l1Contracts.receiver.proxy), l1Contracts.receiver.proxyAdmin, "test_Deploy::9");
+            assertEq(ProxyAdmin(l1Contracts.receiver.proxyAdmin).owner(), deployer, "test_Deploy::10");
+            assertEq(
+                _getProxyImplementation(l1Contracts.receiver.proxy),
+                l1Contracts.receiver.implementation,
+                "test_Deploy::11"
+            );
+
+            CCIPAdapter ccipAdapter = CCIPAdapter(l1Contracts.arbitrumAdapter);
+
+            assertEq(ccipAdapter.LINK_TOKEN(), address(0), "test");
+            assertEq(ccipAdapter.CCIP_ROUTER(), ETHEREUM_CCIP_ROUTER, "test_Deploy::12");
+            assertEq(ccipAdapter.L1_TOKEN(), ETHEREUM_EGETH_TOKEN, "test_Deploy::13");
+        }
+
+        vm.selectFork(script.arbitrumForkId());
+
+        {
+            PriceOracle priceOracle = PriceOracle(l2Contracts[0].priceOracle);
+
+            (address dataFeed, bool isInverse, uint32 heartbeat, uint8 decimals) = priceOracle.getOracleParameters();
+
+            assertEq(dataFeed, ARBITRUM_EGETH_ETH_DATAFEED, "test_Deploy::18");
+            assertEq(isInverse, ARBITRUM_EGETH_ETH_DATAFEED_IS_INVERSE, "test_Deploy::19");
+            assertEq(heartbeat, ARBITRUM_EGETH_ETH_DATAFEED_HEARTBEAT, "test_Deploy::20");
+            assertEq(AggregatorV3Interface(dataFeed).decimals(), decimals, "test_Deploy::21");
+
+            OraclePool oraclePool = OraclePool(l2Contracts[0].oraclePool);
+
+            assertEq(oraclePool.SENDER(), l2Contracts[0].sender.proxy, "test_Deploy::22");
+            assertEq(oraclePool.TOKEN_IN(), ARBITRUM_WETH_TOKEN, "test_Deploy::23");
+            assertEq(oraclePool.TOKEN_OUT(), ARBITRUM_EGETH_TOKEN, "test_Deploy::24");
+            assertEq(oraclePool.getOracle(), l2Contracts[0].priceOracle, "test_Deploy::25");
+            assertEq(oraclePool.getFee(), ARBITRUM_ORACLE_POOL_FEE, "test_Deploy::26");
+            assertEq(oraclePool.owner(), deployer, "test_Deploy::27");
+
+            CustomSender sender = CustomSender(l2Contracts[0].sender.proxy);
+
+            assertEq(sender.WNATIVE(), ARBITRUM_WETH_TOKEN, "test_Deploy::28");
+            assertEq(sender.LINK_TOKEN(), ARBITRUM_LINK_TOKEN, "test_Deploy::29");
+            assertEq(sender.CCIP_ROUTER(), ARBITRUM_CCIP_ROUTER, "test_Deploy::30");
+            assertEq(sender.getOraclePool(), l2Contracts[0].oraclePool, "test_Deploy::31");
+            assertEq(sender.hasRole(sender.DEFAULT_ADMIN_ROLE(), deployer), true, "test_Deploy::32");
+            assertEq(sender.hasRole(sender.SYNC_ROLE(), l2Contracts[0].syncAutomation), true, "test_Deploy::33");
+            assertEq(
+                abi.decode(sender.getReceiver(ETHEREUM_CCIP_CHAIN_SELECTOR), (address)),
+                l1Contracts.receiver.proxy,
+                "test_Deploy::34"
+            );
+            assertEq(_getProxyAdmin(l2Contracts[0].sender.proxy), l2Contracts[0].sender.proxyAdmin, "test_Deploy::35");
+            assertEq(ProxyAdmin(l2Contracts[0].sender.proxyAdmin).owner(), deployer, "test_Deploy::36");
+            assertEq(
+                _getProxyImplementation(l2Contracts[0].sender.proxy),
+                l2Contracts[0].sender.implementation,
+                "test_Deploy::37"
+            );
+
+            SyncAutomation syncAutomation = SyncAutomation(l2Contracts[0].syncAutomation);
+
+            assertEq(syncAutomation.SENDER(), l2Contracts[0].sender.proxy, "test_Deploy::38");
+            assertEq(syncAutomation.DEST_CHAIN_SELECTOR(), ETHEREUM_CCIP_CHAIN_SELECTOR, "test_Deploy::39");
+            assertEq(syncAutomation.WNATIVE(), ARBITRUM_WETH_TOKEN, "test_Deploy::40");
+            assertEq(syncAutomation.owner(), deployer, "test_Deploy::41");
+            assertEq(syncAutomation.getLastExecution(), block.timestamp, "test_Deploy::42");
+            assertEq(syncAutomation.getDelay(), type(uint48).max, "test_Deploy::43");
+        }
+    }
+
+    function _getProxyAdmin(address proxy) internal view returns (address) {
+        return address(uint160(uint256(vm.load(proxy, ERC1967Utils.ADMIN_SLOT))));
+    }
+
+    function _getProxyImplementation(address proxy) internal view returns (address) {
+        return address(uint160(uint256(vm.load(proxy, ERC1967Utils.IMPLEMENTATION_SLOT))));
+    }
+}
