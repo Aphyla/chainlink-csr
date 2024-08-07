6a27a90693e9a168f426f13dc9de1ddefedf566b
Aphyla <aphyla@aphyla.com>
2024-08-07 16:39:00 +0200

    Fix typo in eigenpie test

--- test/script/eigenpie/EigenpieDeployScript.t.sol
+++ test/script/eigenpie/EigenpieDeployScript.t.sol
@@ -32,21 +32,21 @@ contract EigenpieDeployScriptTest is Test, EigenpieParameters {
             assertEq(
                 receiver.getSender(ARBITRUM_CCIP_CHAIN_SELECTOR),
                 abi.encode(l2Contracts[0].sender.proxy),
-                "test_Deploy::7"
+                "test_Deploy::6"
             );
-            assertEq(_getProxyAdmin(l1Contracts.receiver.proxy), l1Contracts.receiver.proxyAdmin, "test_Deploy::9");
-            assertEq(ProxyAdmin(l1Contracts.receiver.proxyAdmin).owner(), deployer, "test_Deploy::10");
+            assertEq(_getProxyAdmin(l1Contracts.receiver.proxy), l1Contracts.receiver.proxyAdmin, "test_Deploy::7");
+            assertEq(ProxyAdmin(l1Contracts.receiver.proxyAdmin).owner(), deployer, "test_Deploy::8");
             assertEq(
                 _getProxyImplementation(l1Contracts.receiver.proxy),
                 l1Contracts.receiver.implementation,
-                "test_Deploy::11"
+                "test_Deploy::9"
             );
 
             CCIPAdapter ccipAdapter = CCIPAdapter(l1Contracts.arbitrumAdapter);
 
-            assertEq(ccipAdapter.LINK_TOKEN(), address(0), "test");
-            assertEq(ccipAdapter.CCIP_ROUTER(), ETHEREUM_CCIP_ROUTER, "test_Deploy::12");
-            assertEq(ccipAdapter.L1_TOKEN(), ETHEREUM_EGETH_TOKEN, "test_Deploy::13");
+            assertEq(ccipAdapter.LINK_TOKEN(), address(0), "test_Deploy::10");
+            assertEq(ccipAdapter.CCIP_ROUTER(), ETHEREUM_CCIP_ROUTER, "test_Deploy::11");
+            assertEq(ccipAdapter.L1_TOKEN(), ETHEREUM_EGETH_TOKEN, "test_Deploy::12");
         }
 
         vm.selectFork(script.arbitrumForkId());
@@ -56,49 +56,49 @@ contract EigenpieDeployScriptTest is Test, EigenpieParameters {
 
             (address dataFeed, bool isInverse, uint32 heartbeat, uint8 decimals) = priceOracle.getOracleParameters();
 
-            assertEq(dataFeed, ARBITRUM_EGETH_ETH_DATAFEED, "test_Deploy::18");
-            assertEq(isInverse, ARBITRUM_EGETH_ETH_DATAFEED_IS_INVERSE, "test_Deploy::19");
-            assertEq(heartbeat, ARBITRUM_EGETH_ETH_DATAFEED_HEARTBEAT, "test_Deploy::20");
-            assertEq(AggregatorV3Interface(dataFeed).decimals(), decimals, "test_Deploy::21");
+            assertEq(dataFeed, ARBITRUM_EGETH_ETH_DATAFEED, "test_Deploy::13");
+            assertEq(isInverse, ARBITRUM_EGETH_ETH_DATAFEED_IS_INVERSE, "test_Deploy::14");
+            assertEq(heartbeat, ARBITRUM_EGETH_ETH_DATAFEED_HEARTBEAT, "test_Deploy::15");
+            assertEq(AggregatorV3Interface(dataFeed).decimals(), decimals, "test_Deploy::16");
 
             OraclePool oraclePool = OraclePool(l2Contracts[0].oraclePool);
 
-            assertEq(oraclePool.SENDER(), l2Contracts[0].sender.proxy, "test_Deploy::22");
-            assertEq(oraclePool.TOKEN_IN(), ARBITRUM_WETH_TOKEN, "test_Deploy::23");
-            assertEq(oraclePool.TOKEN_OUT(), ARBITRUM_EGETH_TOKEN, "test_Deploy::24");
-            assertEq(oraclePool.getOracle(), l2Contracts[0].priceOracle, "test_Deploy::25");
-            assertEq(oraclePool.getFee(), ARBITRUM_ORACLE_POOL_FEE, "test_Deploy::26");
-            assertEq(oraclePool.owner(), deployer, "test_Deploy::27");
+            assertEq(oraclePool.SENDER(), l2Contracts[0].sender.proxy, "test_Deploy::17");
+            assertEq(oraclePool.TOKEN_IN(), ARBITRUM_WETH_TOKEN, "test_Deploy::18");
+            assertEq(oraclePool.TOKEN_OUT(), ARBITRUM_EGETH_TOKEN, "test_Deploy::19");
+            assertEq(oraclePool.getOracle(), l2Contracts[0].priceOracle, "test_Deploy::20");
+            assertEq(oraclePool.getFee(), ARBITRUM_ORACLE_POOL_FEE, "test_Deploy::21");
+            assertEq(oraclePool.owner(), deployer, "test_Deploy::22");
 
             CustomSender sender = CustomSender(l2Contracts[0].sender.proxy);
 
-            assertEq(sender.WNATIVE(), ARBITRUM_WETH_TOKEN, "test_Deploy::28");
-            assertEq(sender.LINK_TOKEN(), ARBITRUM_LINK_TOKEN, "test_Deploy::29");
-            assertEq(sender.CCIP_ROUTER(), ARBITRUM_CCIP_ROUTER, "test_Deploy::30");
-            assertEq(sender.getOraclePool(), l2Contracts[0].oraclePool, "test_Deploy::31");
-            assertEq(sender.hasRole(sender.DEFAULT_ADMIN_ROLE(), deployer), true, "test_Deploy::32");
-            assertEq(sender.hasRole(sender.SYNC_ROLE(), l2Contracts[0].syncAutomation), true, "test_Deploy::33");
+            assertEq(sender.WNATIVE(), ARBITRUM_WETH_TOKEN, "test_Deploy::23");
+            assertEq(sender.LINK_TOKEN(), ARBITRUM_LINK_TOKEN, "test_Deploy::24");
+            assertEq(sender.CCIP_ROUTER(), ARBITRUM_CCIP_ROUTER, "test_Deploy::25");
+            assertEq(sender.getOraclePool(), l2Contracts[0].oraclePool, "test_Deploy::26");
+            assertEq(sender.hasRole(sender.DEFAULT_ADMIN_ROLE(), deployer), true, "test_Deploy::27");
+            assertEq(sender.hasRole(sender.SYNC_ROLE(), l2Contracts[0].syncAutomation), true, "test_Deploy::28");
             assertEq(
                 abi.decode(sender.getReceiver(ETHEREUM_CCIP_CHAIN_SELECTOR), (address)),
                 l1Contracts.receiver.proxy,
-                "test_Deploy::34"
+                "test_Deploy::29"
             );
-            assertEq(_getProxyAdmin(l2Contracts[0].sender.proxy), l2Contracts[0].sender.proxyAdmin, "test_Deploy::35");
-            assertEq(ProxyAdmin(l2Contracts[0].sender.proxyAdmin).owner(), deployer, "test_Deploy::36");
+            assertEq(_getProxyAdmin(l2Contracts[0].sender.proxy), l2Contracts[0].sender.proxyAdmin, "test_Deploy::30");
+            assertEq(ProxyAdmin(l2Contracts[0].sender.proxyAdmin).owner(), deployer, "test_Deploy::31");
             assertEq(
                 _getProxyImplementation(l2Contracts[0].sender.proxy),
                 l2Contracts[0].sender.implementation,
-                "test_Deploy::37"
+                "test_Deploy::32"
             );
 
             SyncAutomation syncAutomation = SyncAutomation(l2Contracts[0].syncAutomation);
 
-            assertEq(syncAutomation.SENDER(), l2Contracts[0].sender.proxy, "test_Deploy::38");
-            assertEq(syncAutomation.DEST_CHAIN_SELECTOR(), ETHEREUM_CCIP_CHAIN_SELECTOR, "test_Deploy::39");
-            assertEq(syncAutomation.WNATIVE(), ARBITRUM_WETH_TOKEN, "test_Deploy::40");
-            assertEq(syncAutomation.owner(), deployer, "test_Deploy::41");
-            assertEq(syncAutomation.getLastExecution(), block.timestamp, "test_Deploy::42");
-            assertEq(syncAutomation.getDelay(), type(uint48).max, "test_Deploy::43");
+            assertEq(syncAutomation.SENDER(), l2Contracts[0].sender.proxy, "test_Deploy::33");
+            assertEq(syncAutomation.DEST_CHAIN_SELECTOR(), ETHEREUM_CCIP_CHAIN_SELECTOR, "test_Deploy::34");
+            assertEq(syncAutomation.WNATIVE(), ARBITRUM_WETH_TOKEN, "test_Deploy::35");
+            assertEq(syncAutomation.owner(), deployer, "test_Deploy::36");
+            assertEq(syncAutomation.getLastExecution(), block.timestamp, "test_Deploy::37");
+            assertEq(syncAutomation.getDelay(), type(uint48).max, "test_Deploy::38");
         }
     }
 
