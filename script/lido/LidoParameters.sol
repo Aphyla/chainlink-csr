ae345982d091c383ed5606b98c7a648402f657f3
Aphyla <aphyla@aphyla.com>
2024-09-22 00:39:09 +0200

    Set fee to 0

--- script/lido/LidoParameters.sol
+++ script/lido/LidoParameters.sol
@@ -28,7 +28,7 @@ contract LidoParameters {
     /* Data feed parameters */
     bool internal constant ARBITRUM_WSTETH_STETH_DATAFEED_IS_INVERSE = false; // If the data feed is inverted, i.e. the price returned is the inverse of the price wanted
     uint32 internal constant ARBITRUM_WSTETH_STETH_DATAFEED_HEARTBEAT = 24 hours; // The maximum time between data feed updates
-    uint96 internal constant ARBITRUM_ORACLE_POOL_FEE = 0.01e18; // The fee to be applied to each swap (in 1e18 scale). It should be set following the rebase APR to prevent any exploit of a slow data feed update and it should also be used to cover the actual gas cost of the sync automation contract
+    uint96 internal constant ARBITRUM_ORACLE_POOL_FEE = 0; // The fee to be applied to each swap (in 1e18 scale). It should be set following the rebase APR to prevent any exploit of a slow data feed update and it should also be used to cover the actual gas cost of the sync automation contract
     uint128 internal constant ARBITRUM_ORIGIN_MAX_SUBMISSION_COST = 0.1e18; // The maximum amount of ETH to be paid for submitting the ticket to the arbitrum bridge
     /* Destination to Origin Fee Parameters */
     uint32 internal constant ARBITRUM_ORIGIN_MAX_GAS = 100_000; // The maximum amount of gas used to cover the L2 execution cost
@@ -49,7 +49,7 @@ contract LidoParameters {
     /* Data feed parameters */
     bool internal constant OPTIMISM_WSTETH_STETH_DATAFEED_IS_INVERSE = false; // If the data feed is inverted, i.e. the price returned is the inverse of the price wanted
     uint32 internal constant OPTIMISM_WSTETH_STETH_DATAFEED_HEARTBEAT = 24 hours; // The maximum time between data feed updates
-    uint96 internal constant OPTIMISM_ORACLE_POOL_FEE = 0.01e18; // The fee to be applied to each swap (in 1e18 scale). It should be set following the rebase APR to prevent any exploit of a slow data feed update and it should also be used to cover the actual gas cost of the sync automation contract
+    uint96 internal constant OPTIMISM_ORACLE_POOL_FEE = 0; // The fee to be applied to each swap (in 1e18 scale). It should be set following the rebase APR to prevent any exploit of a slow data feed update and it should also be used to cover the actual gas cost of the sync automation contract
     /* Destination to Origin Fee Parameters */
     uint32 internal constant OPTIMISM_ORIGIN_L2_GAS = 100_000; // The amount of gas used to cover the L2 execution cost
     /* Sync Automation Parameters */
@@ -68,7 +68,7 @@ contract LidoParameters {
     /* Data feed parameters */
     bool internal constant BASE_WSTETH_STETH_DATAFEED_IS_INVERSE = false; // If the data feed is inverted, i.e. the price returned is the inverse of the price wanted
     uint32 internal constant BASE_WSTETH_STETH_DATAFEED_HEARTBEAT = 24 hours; // The maximum time between data feed updates
-    uint96 internal constant BASE_ORACLE_POOL_FEE = 0.01e18; // The fee to be applied to each swap (in 1e18 scale). It should be set following the rebase APR to prevent any exploit of a slow data feed update and it should also be used to cover the actual gas cost of the sync automation contract
+    uint96 internal constant BASE_ORACLE_POOL_FEE = 0; // The fee to be applied to each swap (in 1e18 scale). It should be set following the rebase APR to prevent any exploit of a slow data feed update and it should also be used to cover the actual gas cost of the sync automation contract
     /* Destination to Origin Fee Parameters */
     uint32 internal constant BASE_ORIGIN_L2_GAS = 100_000; // The amount of gas used to cover the L2 execution cost
     /* Sync Automation Parameters */
