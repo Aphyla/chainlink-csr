ec7b82ae488527712a9cb7af869b386c98f0c9a9
Aphyla <aphyla@aphyla.com>
2025-07-17 13:25:42 +0200

    Refactor LineaAdapterL1toL2

--- contracts/adapters/LineaAdapterL1toL2.sol
+++ contracts/adapters/LineaAdapterL1toL2.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import {BridgeAdapter} from "./BridgeAdapter.sol";
-import {FeeCodec} from "../libraries/FeeCodec.sol";
 import {ILineaTokenBridge} from "../interfaces/ILineaTokenBridge.sol";
+import {FeeCodec} from "../libraries/FeeCodec.sol";
+import {BridgeAdapter} from "./BridgeAdapter.sol";
 
 /**
  * @title LineaAdapterL1toL2 Contract
@@ -47,10 +47,11 @@ contract LineaAdapterL1toL2 is BridgeAdapter {
         (uint256 feeAmount, bool payInLink) = FeeCodec.decodeLineaL1toL2(feeData);
 
         if (payInLink) revert LineaAdapterL1toL2InvalidFeeToken();
+        if (feeAmount != 0) revert LineaAdapterL1toL2InvalidFeeAmount(feeAmount, 0);
 
         IERC20(TOKEN).forceApprove(TOKEN_BRIDGE, amount);
 
-        ILineaTokenBridge(TOKEN_BRIDGE).bridgeToken{value: feeAmount}(TOKEN, amount, to);
+        ILineaTokenBridge(TOKEN_BRIDGE).bridgeToken(TOKEN, amount, to);
 
         emit LineaL1toL2MessageSent();
     }
